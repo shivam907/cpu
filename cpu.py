0270466c16d206f20114865d6a4d6765a88ab2ac
shivam907 <60144462+shivam907@users.noreply.github.com>
2020-07-28 00:54:30 +0530

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -3,6 +3,6 @@ def cpu():
     usage = str(psutil.cpu_percent())
     print('CPU is at '+ usage)
     battery = psutil.sensors_battery()
-    print("Battery is at")
+    print("Battery is at ")
     print(battery.percent )
 cpu()
